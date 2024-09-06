0637007c804f80265712ae9aeaf9b8534615bbc2
Ryder <CarameL1345@outlook.com>
2024-09-06 14:59:26 -0600

    Game object transformation challenge complete

--- AdventureGame/GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
+++ AdventureGame/GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -5,11 +5,11 @@ public class TransformController : MonoBehaviour
     private void Update()
     {
         // Move the target GameObject
-        var x = Mathf.PingPong(Time.time, 3);
-        var p = new Vector3(0, x, 0);
+        var x = Mathf.PingPong(Time.time, 10);
+        var p = new Vector3(x, 0, 0);
         transform.position = p;
 
         // Rotate the target GameObject
-        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 0, 1000) * Time.deltaTime);
     }
 }
