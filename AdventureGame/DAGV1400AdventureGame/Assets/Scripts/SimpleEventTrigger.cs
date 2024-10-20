ebc7f709539caa9e4cd1969e045372bb4ec48cd7
Ryder <CarameL1345@outlook.com>
2024-10-19 21:24:11 -0600

    scripting 6 challenge

--- AdventureGame/DAGV1400AdventureGame/Assets/Scripts/SimpleEventTrigger.cs
+++ AdventureGame/DAGV1400AdventureGame/Assets/Scripts/SimpleEventTrigger.cs
@@ -5,11 +5,13 @@ public class SimpleEventTrigger : MonoBehaviour
 {
 
     public UnityEvent triggerEvent;
+    public Animator animator;
 
     private void OnTriggerEnter(Collider other)
     {
         // trigger the event and test with a debug mssg
         triggerEvent.Invoke();
         Debug.Log("Player interacted with the object");
+        animator.SetTrigger("Hit");
     }
 }
