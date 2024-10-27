68101671e6824615df47d221a935bfd5435ec561
Ryder <CarameL1345@outlook.com>
2024-10-27 15:26:20 -0600

    Challenge 8

--- AdventureGame/DAGV1400AdventureGame/Assets/Scripts/CollectibleTrigger.cs
+++ AdventureGame/DAGV1400AdventureGame/Assets/Scripts/CollectibleTrigger.cs
@@ -1,14 +1,30 @@
 using UnityEngine;
 using UnityEngine.Events;
+using System.Collections;
 
 public class CollectibleTrigger : MonoBehaviour
 {
 
     public UnityEvent triggerEvent;
+    public Animator animator;
 
     private void OnTriggerEnter(Collider other)
     {
         // trigger the event and test with a debug mssg
         triggerEvent.Invoke();
+        
+        StartCoroutine(PlayAnimationAndDestroy());
+    }
+
+    private IEnumerator PlayAnimationAndDestroy()
+    {
+        // Trigger the animation
+        animator.SetTrigger("Collected");
+
+        // Wait for the animation to complete (adjust the duration as necessary)
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+
+        // Destroy the object after the animation
+        Destroy(gameObject);
     }
 }
